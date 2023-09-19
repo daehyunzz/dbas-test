4048a7abb263e3d4335d70ea3e7123cecfbabb3e
plastic041 <plastic041@gmail.com>
2023-09-19 11:16:31 +0900

    feat: components

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,4 @@
+import MuiButton from '@mui/material/Button';
+import { styled } from '@mui/system';
+
+export const Button = styled(MuiButton)();
---
+++ src/components/ui/TextField.tsx
@@ -0,0 +1,4 @@
+import MuiTextField from '@mui/material/TextField';
+import { styled } from '@mui/system';
+
+export const TextField = styled(MuiTextField)();
