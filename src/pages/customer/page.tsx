61bb75d401147c0a49e0a554b5860b990ff4e3a2
jonghyunChoi <bluecoolgod80@gmail.com>
2023-09-20 15:05:48 +0900

    고객조회 화면 레이아웃 완료

--- src/pages/customer/page.tsx
+++ src/pages/customer/page.tsx
@@ -1,8 +1,9 @@
 import { Box, Input, Select, Typography } from '@mui/material';
 
+import { FigmaButton } from '@components/CustomerTables/FigmaButton';
 import { FigmaColorBox } from '@components/CustomerTables/FigmaColorBox';
 import FigmaTable, { Column } from '@components/CustomerTables/FigmaTable';
-import { Button } from '@components/ui/Button';
+import { MenuItem } from '@components/ui/MenuItem';
 
 export const Customer = () => {
     const columns: Column[] = [
@@ -48,11 +49,20 @@ export const Customer = () => {
         let status;
 
         if (i % 3 === 0) {
-            status = <FigmaColorBox text={'잠재고객'} />;
+            status = <FigmaColorBox width="88.32px" height="35.2px" text={'잠재고객'} />;
         } else if (i % 3 === 1) {
-            status = '일반';
+            status = (
+                <FigmaColorBox
+                    width="88.32px"
+                    height="35.2px"
+                    color="LightBlue"
+                    text={'일반고객'}
+                />
+            );
         } else {
-            status = '충성';
+            status = (
+                <FigmaColorBox width="88.32px" height="35.2px" color="Yellow" text={'충성고객'} />
+            );
         }
 
         const rowData: CustomerRow = {
@@ -69,18 +79,88 @@ export const Customer = () => {
         rows.push(rowData);
     }
     return (
-        <Box>
-            <Box>고객조회</Box>
-            <Input placeholder="검색어를 입력해주세요." />
-            <Box>
-                <Typography>총 356명(잠재213/일반106/충성37)</Typography>
+        <Box sx={{ width: '100%', padding: '50px' }}>
+            <Box
+                sx={{
+                    width: '100%',
+                    height: '60px',
+                    borderBottom: '1px solid #A9A9A9',
+                    marginBottom: '40px',
+                }}
+            >
+                <Typography
+                    sx={{
+                        // width: '127px',
+                        height: '38px',
+                        fontWeight: '700',
+                        fontSize: '32px',
+                        lineHeight: '37.5px',
+                    }}
+                >
+                    고객조회
+                </Typography>
             </Box>
-            <Box>
-                <Select value={'전체'}></Select>
-                <Button>엑셀로 추출하기</Button>
-            </Box>
-            <Box>
-                <FigmaTable columns={columns} rows={rows} isCheckbox={false} />
+            <Box sx={{ display: 'flex', gap: '30px', flexFlow: 'column' }}>
+                <Input
+                    placeholder="검색어를 입력해주세요."
+                    sx={{
+                        width: '843px',
+                        height: '64px',
+                        fontWeight: '400',
+                        fontSize: '28px',
+                        lineHeight: '32.81px',
+                        border: 'solid 1px #000000',
+                    }}
+                />
+                <Box>
+                    <Typography sx={{ fontWeight: '700', fontSize: '32px', lineHeight: '37.5px' }}>
+                        총 {rows.length}명(잠재
+                        {
+                            rows.filter((e) => {
+                                //@ts-expect-error e.status가 JSX.Element임
+                                return e.status?.props?.text === '잠재고객';
+                            }).length
+                        }
+                        /일반
+                        {
+                            rows.filter((e) => {
+                                //@ts-expect-error e.status가 JSX.Element임
+                                return e.status?.props?.text === '일반고객';
+                            }).length
+                        }
+                        /충성
+                        {
+                            rows.filter((e) => {
+                                //@ts-expect-error e.status가 JSX.Element임
+                                return e.status?.props?.text === '충성고객';
+                            }).length
+                        }
+                        )
+                    </Typography>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Select
+                        value={'전체'}
+                        sx={{
+                            width: '172px',
+                            height: '62px',
+                            fontWeight: '400',
+                            fontSize: '28px',
+                            lineHeight: '32.81px',
+                        }}
+                    >
+                        <MenuItem value={'전체'}>전체</MenuItem>
+                    </Select>
+                    <Box sx={{ display: 'flex', gap: '1rem' }}>
+                        <FigmaButton text="엑셀로 추출하기" type="white" width="262px" />
+                        <FigmaButton text="대량 등록 템플릿" type="white" width="269px" />
+                        <FigmaButton text="대량 등록" type="blue" width="144px" />
+                        <FigmaButton text="개별 등록" type="blue" width="144px" />
+                    </Box>
+                </Box>
+                <Box>
+                    <FigmaTable columns={columns} rows={rows} isCheckbox={false} />
+                </Box>
             </Box>
         </Box>
     );
